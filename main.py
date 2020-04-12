307f28300acd7abbf52c5cb8bb422c8ae01210ad
Roberto Henriquez Perozo <rihp94@gmail.com>
2020-04-12 12:40:46 +0200

    implemented main.py filtering by year and month arguments

--- main.py
+++ main.py
@@ -14,7 +14,12 @@ print('hi, welcome to vesuvius alpha!')
 ########################################################################################################
 
 
-# 01 - INSTRUCTIONS AND ARGUMENTS
+# 01 - INPUTS AND OUTPUTS PATHS
+df = pd.read_csv('OUTPUT/volcanic-eruptions.csv')
+########################################################################################################
+
+
+# 02 - INSTRUCTIONS AND ARGUMENTS
 parser = argparse.ArgumentParser(description="Vesuvius is a retriever of satellite images. It currently uses a dataset containing volcanic eruptions and matches it with available photos from NASA's DSCOVR satellite, using the 'EPIC' API. The functionality of vesuvius has been desinged to be a modular program, in the sense that one could input a different dataset with relevant dates, and get the available satellite images from that date; Note that the DSCOVR satellite was launched into space on 2015, so older data will not be available. Also, very recent dates may not be available in the API's archive. DISCLAIMER: Use at your own risk.")
 ## year
 parser.add_argument('year', help="This will be the year to filter. The correct input format: YYYY")
@@ -28,18 +33,14 @@ parser.add_argument('--version', help="displays vesuvius' version")
 # parser.add_argument('--update', help="functionality under development, action='store_true'")
 ########################################################################################################
 
-# 02 -PARSE ARGS AND CATCH ERRORS
+# 03 -PARSE ARGS AND CATCH ERRORS
 args = parser.parse_args()
 if len(args.year) != 4:
     raise ValueError('The year has a wrong format. Try again using YYYY')
 if len(args.month) != 2:
     raise ValueError('The year has a wrong format. Try again using MM')
 ########################################################################################################
-
-
-# 03 - INPUTS AND OUTPUTS PATHS
-#df = pd.read_csv('OUTPUT/volcanic-explosions.csv')
-########################################################################################################
+print('STEP 03 DONE\n')
 
 # 04 - UPDATE
 ## Run data cleaner
@@ -50,6 +51,9 @@ if len(args.month) != 2:
 
 # 05 - FILTER available data with arguments
 print(f'Analyzing date YYYY-MM: {str(args.year)}-{str(args.month)}')
+df_filtered = df[df.start_y == int(args.year)][df.start_m == int(args.month)]
+print(df_filtered)
+print(f"Resulting shape of the DataFrame{df_filtered.shape}")
 ########################################################################################################
 
 # 06 - SUMMARY
