a31d79a858cc857c19ab969c1f1eb8de6a66c766
Roberto Henriquez Perozo <rihp94@gmail.com>
2020-04-13 17:29:10 +0200

    refactoring main.py

---
+++ modules/config.py
@@ -0,0 +1,9 @@
+# 01(a) - INPUT Paths
+raw_dataset_path = 'INPUT/volcanic_dataset.xls' # RAW Data Source: SMITHSONIAN INSTITUTE or https://www.kaggle.com/martincontreras/volcanic-eruptions-dataset-all-to-2020
+cached_data_csv_path = 'OUTPUT/enriched-data-cache.csv'
+report_cache_csv_path = 'OUTPUT/report-cache.csv'
+
+# 01(B) - OUTPUT Paths
+output_csv_path = 'OUTPUT/enriched-data.csv'
+pdf_output_path = 'OUTPUT/generated-pdf.pdf'
+# Image outputs can be configured here
