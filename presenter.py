f4e909c7b1639cefdb42cad6bea63bc5b6b19e13
Roberto Henriquez Perozo <rihp94@gmail.com>
2020-04-13 13:15:15 +0200

    presenter.py generates a normal table of occuring events

--- presenter.py
+++ presenter.py
@@ -6,7 +6,9 @@ def generate_report(df, output_pdf_path, kwargs):
     """
     A script that takes a dataframe and outputs the relevant data as a pdf
     """
-
+    # Define relevant columns in df
+    df_unfiltered = df
+    df = df[['v_name', 'start_y', 'start_m', 'start_d', 'delta', 'lat', 'lon', 'sat_lats', 'sat_lons']]
     pdf = FPDF("P","mm","A4")
     pdf.add_page()
     def page_header():
@@ -19,17 +21,85 @@ def generate_report(df, output_pdf_path, kwargs):
     # Generate landing page
     page_header()
     pdf.cell(0, 8, f'This report was generated on: {datetime.datetime.today()}',0,1)
-
-    # Specify the requested dates for the report
     pdf.cell(0, 8, f"Requested date(YYYY-MM):   {kwargs['requested_year']}-{kwargs['requested_month']}",0,1)
-
-    
     pdf.cell(0, 8, f'Available data: {df.shape[0]} rows and {df.shape[1]} columns.',0,1)
+    
     pdf.cell(0, 8, f'Volcanoes with registered eruptions on requested date:',0,1)
-
     for v_name in df.v_name:
-        pdf.cell(0, 8, f'   - {v_name}',0,1)
+        pdf.cell(0, 8, f'     - {v_name}',0,1)
 
+    def tableColumns(df):
+        for col in df.columns:
+            yield col
+
+    def tableValues(df):
+        for _,row in df.iterrows():
+            yield row
+
+    pdf.cell(0, 8, f'{list(tableColumns(df))}',0,1)
+
+    # PDFCONFIG.PY
+    # Dictionaries for 2 font settings and 3 color settings
+    # Usefull for changing settings without having to go through code.
+    font_type_table_header = {"family":"Arial", "style":"B", "size":10}
+    font_type_table_body = {"family":"Arial", "style":"", "size":8}
+    black = (0,-1,-1)
+    green = (10,188,87)
+    yellow = (250,239,133)
+
+
+    #MAKEPRETTY.PY
+    # Create an instance of `FPDF` to use it's methods
+    # It is created outside of the function, because otherwise
+    # it would generate a new object every time you called `fit_word`
+    ver = FPDF()
+
+    def fit_word(string,cell_w,font_type):
+        string=str(string)
+        ver.set_font(**font_type)
+        width = ver.get_string_width(string)
+        if ver.get_string_width(string)<cell_w:
+            return string
+        while ver.get_string_width(string)>=cell_w:
+            string = string[:-1]
+        string = string[:-3] + "..."
+        return string
+
+
+    # Define our cols and num_cols so cell width is calculated relatively
+    # You can change the number of columns and it will adapt to fit
+    # the whole page (190 mm = 210mm - 2 * 10mm)
+    # 10mm is the default left and right margins
+    cols = list(tableColumns(df))
+    num_cols = len(cols)
+    A4_width = 210
+    l_margin = 10
+    r_margin = 10
+    useful_page_width = A4_width-l_margin-r_margin
+
+    # Use font_type_table_header and font_type_table_body
+    # to get font configuration from other file `config.py`
+    # so it can be changed without going through code.
+    # The same can be applied to color, etc.
+    pdf.set_font(**font_type_table_header)
+    pdf.set_draw_color(0)
+    pdf.set_text_color(0)
+
+    cell_width = useful_page_width/num_cols
+
+    # Printing Header
+    for col in cols:
+        pdf.cell(cell_width,10,col.upper(),1,0,"C")
+    # Add line break after printing whole line
+    pdf.ln()
+
+    # Printing rows
+    pdf.set_font(**font_type_table_body)
+    pdf.set_fill_color(255)
+    for row in tableValues(df): 
+        for val in row:
+            pdf.cell(cell_width,10,fit_word(val,cell_width,font_type_table_body),1,0,"C",1)
+        pdf.ln()
 
 
     def new_event_page(pandas_row):
